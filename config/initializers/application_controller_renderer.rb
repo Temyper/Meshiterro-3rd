8b467136cf17bb6c0c5145907cba48d9a9638aba
Temyper <pall47g1@gmail.com>
2020-12-14 14:41:48 +0000

    [Finish] chapter-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '6acda2370a2a7ccacc41a53728236ad5920a6361ac3f8b789653825319f144e39a2e39aa6192fd3c69494a28666bce93b89f19b8a8ec9e6d7aecc5f1c984bf40'
