0bbfc3211335d168e285310b390f6e39daf128f6
Temyper <pall47g1@gmail.com>
2020-12-14 15:08:32 +0000

    [Finish] chapter-4

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   # devise_for :users
   # # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root to: 'homes#top'
-  root 'post_images#index'
   devise_for :users
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  root 'post_images#index'
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
 end
